f646150f4331add1de4e24887f25be6f0e81162b
Young jun Ju <juju21@hanmail.net>
2024-10-12 21:41:46 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
+import dayjs from "dayjs";
 import Image from "next/image";
+import Link from "next/link";
+import { PrevButton } from "./PrevButton";
+import { NextButton } from "./NextButton";
 
-export default function Home() {
+type Props = {
+  searchParams: {
+    targetDt?: string;
+  }
+}
+
+const today = dayjs().subtract(1, 'day').format('YYYYMMDD');
+
+export default async function Home({searchParams: { targetDt = today } }:Props) {
+
+  const key = '38822ae27494db7feee3341504461c69';
+  const baseUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json';
+  const url = `${baseUrl}?key=${key}&targetDt=${targetDt}`;
+
+  const response = await fetch(url);
+  const json: ResponseType = await response.json();
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="w-[500px] mx-auto">
+      <div className="flex justify-between">
+        {/* <a href={'?targetDt=' + dayjs(targetDt).subtract(1, 'day').format('YYYYMMDD')}>이전</a> */}
+        {/* <Link></Link> */}
+        {/* <button>이전</button> */}
+        <PrevButton targetDt={targetDt}/>
+        {dayjs(targetDt).format('YYYY년 MM월 DD일')}
+        <NextButton targetDt={targetDt}/>
+      </div>
+      <ol className="divide-y *:py-4 mt-4">
+        {
+          json.boxOfficeResult.dailyBoxOfficeList.map((item: ItemType) => (
+            <li key={item.rank}>{item.rank}위 -{' '}
+                {item.movieNm}
+              { item.rankOldAndNew === 'NEW' && <span className="ml-1 text-xs text-red-500">N</span> }
+            </li>
+          ))
+        }
+      </ol>
     </div>
   );
+}
+
+type ResponseType = {
+  boxOfficeResult: {
+    boxofficeType: string,
+    showRange: string,
+    dailyBoxOfficeList: ItemType[],
+  }
+}
+
+type ItemType = {
+  rnum: string;
+  rank: string;
+  rankInten: string;
+  rankOldAndNew: string;
+  movieCd: string;
+  movieNm: string;
+  openDt: string;
+  salesAmt: string;
+  salesShare: string;
+  salesInten: string;
+  salesChange: string;
+  salesAcc: string;
+  audiCnt: string;
+  audiInten: string;
+  audiChange: string;
+  audiAcc: string;
+  scrnCnt: string;
+  showCnt: string;
 }
